6abbf0382fc77840dc34c5dd29bc45fe9c81980a
Patrick Lake <plake492@gmail.com>
2021-03-26 21:28:47 -0400

    Add Nav and About components

---
+++ src/components/Nav.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const Nav = () => {
+  const categories = [
+    {
+      name: 'commercial',
+      description:
+        'Photos of grocery stores, food trucks, and other commercial projects'
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    {
+      name: 'landscape',
+      description: 'Fields, farmhouses, waterfalls, and the beauty of nature'
+    }
+  ]
+
+  const categorySelected = name => {
+    console.log(`${name} clicked`)
+  }
+
+  return (
+    <header className='ml-4'>
+      <h2 style={{ display: 'inline-block' }}>
+        <a href='/'>
+          <span role='img' aria-label='camera'>
+            {' '}
+            📸
+          </span>{' '}
+          Oh Snap!
+        </a>
+      </h2>
+      <nav style={{ display: 'inline-block' }}>
+        <ul className='flex-row'>
+          <li className='mx-2'>
+            <a href='#about'>About me</a>
+          </li>
+          <li>
+            <span>Contact</span>
+          </li>
+          {categories.map(category => (
+            <li className='mx-1' key={category.name}>
+              <span onClick={() => categorySelected(category.name)}>
+                {category.name}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Nav
