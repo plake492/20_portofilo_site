42159b0e8c08777ccbd1465ae24be675bbdd88ef
Patrick Lake <plake492@gmail.com>
2021-03-29 08:31:21 -0400

    Add testing // Gallery // Contact

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
-import './App.css'
-import About from './views/About'
+import React, { useState, useEffect } from 'react'
 import Nav from './components/Nav'
+import About from './views/About'
+import Gallery from './views/Gallery'
+import ContactForm from './views/ContactForm'
+import { capitalizeFirstLetter } from './utils/helpers'
 
-const App = () => {
+function App () {
+  const [categories] = useState([
+    {
+      name: 'commercial',
+      description:
+        'Photos of grocery stores, food trucks, and other commercial projects'
+    },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    {
+      name: 'landscape',
+      description: 'Fields, farmhouses, waterfalls, and the beauty of nature'
+    }
+  ])
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0])
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name)
+  }, [currentCategory])
+
   return (
     <div>
-      <Nav />
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      />
       <main>
-        <About />
+        <div>
+          <ContactForm />
+          <Gallery currentCategory={currentCategory} />
+          <About />
+        </div>
       </main>
     </div>
   )
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,27 +1,10 @@
 import React from 'react'
+import { capitalizeFirstLetter } from '../../utils/helpers'
 
-const Nav = () => {
-  const categories = [
-    {
-      name: 'commercial',
-      description:
-        'Photos of grocery stores, food trucks, and other commercial projects'
-    },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    {
-      name: 'landscape',
-      description: 'Fields, farmhouses, waterfalls, and the beauty of nature'
-    }
-  ]
-
-  const categorySelected = name => {
-    console.log(`${name} clicked`)
-  }
-
+function Nav ({ categories = [], setCurrentCategory, currentCategory }) {
   return (
-    <header className='ml-4'>
-      <h2 style={{ display: 'inline-block' }}>
+    <header className='flex-row px-1'>
+      <h2>
         <a data-testid='link' href='/'>
           <span role='img' aria-label='camera'>
             {' '}
@@ -30,20 +13,29 @@ const Nav = () => {
           Oh Snap!
         </a>
       </h2>
-      <nav style={{ display: 'inline-block' }}>
+      <nav>
         <ul className='flex-row'>
           <li className='mx-2'>
             <a data-testid='about' href='#about'>
               About me
             </a>
           </li>
-          <li>
+          <li className='mx-2'>
             <span>Contact</span>
           </li>
           {categories.map(category => (
-            <li className='mx-1' key={category.name}>
-              <span onClick={() => categorySelected(category.name)}>
-                {category.name}
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && 'navActive'
+              }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category)
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
               </span>
             </li>
           ))}
